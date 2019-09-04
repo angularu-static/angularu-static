4a8575b91be59c97af9baeb7aa96f6bf36eca653
Hasmukh <ahirhasmukh08@outlook.com>
2019-09-04 21:31:47 +0530

    mobile menu added

--- ng/assets/js/custom.js
+++ ng/assets/js/custom.js
@@ -7,14 +7,14 @@ $('document').ready(function(){
         $('.nav--user--list').toggle();
     })
 
-    $('#nav--toggle').click(function() {
-        $('page-nav ul.nav--list').addClass('open');
-        $(this).addClass('open-menu');
-    });
-
     $('body').on('click', '.open-menu', function() {
         debugger
         $('page-nav ul.nav--list').removeClass('open');
         $('#nav--toggle').removeClass('open-menu');
     })
+
+    $('#nav--toggle').click(function() {
+        $('page-nav ul.nav--list').addClass('open');
+        $(this).addClass('open-menu');
+    });
 });
